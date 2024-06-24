a6a963f03ef6bba299e1decea07bc737d4ddc7e2
RWL <rlentz@gmail.com>
2024-06-24 15:45:51 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'support for multistage analysis automation'
+
+setup(
+        name="blne", 
+        version=VERSION,
+        description=DESCRIPTION,
+        packages=find_packages(),
+        url='https://github.com/ricklentz/blne'
+)
